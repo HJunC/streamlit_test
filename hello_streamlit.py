f17237412274780bd9473f7630f8d19799776be9
HJunC <wnscjf9986@gmail.com>
2025-08-12 10:54:21 +0900

    변경

--- hello_streamlit.py
+++ hello_streamlit.py
@@ -10,5 +10,3 @@ st.write("안녕하세요! Streamlit으로 만든 웹 애플리케이션입니�
 # 버튼 추가
 if st.button("클릭해보세요!"):
     st.success("버튼이 클릭되었습니다! 🎊")
-
-st.dataframe(data=None, width=None, height=None, *, use_container_width=None, hide_index=None, column_order=None, column_config=None, key=None, on_select="ignore", selection_mode="multi-row", row_height=None)
